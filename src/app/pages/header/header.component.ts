b628564f6440f71c2b408293843c17d45491adab
KozerenkoRoman <kozerenko@gmail.com>
2022-01-11 00:13:04 +0200

    refactor: join header page with welcome page

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -9,24 +9,11 @@ import {GlobalCoreService} from "../../shared/global-core.service";
 
 export class HeaderComponent implements OnInit {
 
-    public svgColor: string = '#0076bc';
-
     constructor(private globalCoreService: GlobalCoreService) {
 
     }
 
     ngOnInit(): void {
     }
-    @HostListener('window:scroll', ['$event'])
-    onWindowScroll() {
-        let element = document.querySelector('.header__top') as HTMLElement;
-        if (window.scrollY > element.clientHeight) {
-            element.classList.add('header__top_inverse');
-        } else {
-            element.classList.remove('header__top_inverse');
-        }
-    };
-
-
 
 }
--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {GlobalCoreService} from '../../shared/global-core.service';
 
 @Component({
@@ -9,10 +9,22 @@ import {GlobalCoreService} from '../../shared/global-core.service';
 
 export class WelcomeComponent implements OnInit {
 
+    public svgColor: string = '#0076bc';
+
     constructor(private globalCoreService: GlobalCoreService) {
     }
 
     ngOnInit(): void {
     }
+
+    @HostListener('window:scroll', ['$event'])
+    onWindowScroll() {
+        let element = document.querySelector('.header__top') as HTMLElement;
+        if (window.scrollY > element.clientHeight) {
+            element.classList.add('header__top_inverse');
+        } else {
+            element.classList.remove('header__top_inverse');
+        }
+    };
 
 }
