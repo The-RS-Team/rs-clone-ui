8250eed6e7c631a928fee94c834de50775eb9948
KozerenkoRoman <kozerenko@gmail.com>
2022-01-12 23:18:22 +0200

    feat: add AuthFirebaseComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {AuthGithubService} from './auth/auth-github.service';
+import {AuthService} from './auth/auth.service';
 import {HeaderComponent} from './pages/header/header.component';
 import {MatButtonModule} from '@angular/material/button';
 import {SignupComponent} from './pages/signup/signup.component';
@@ -10,7 +10,11 @@ import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {WelcomeComponent} from './pages/welcome/welcome.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {GlobalCoreService} from "./shared/global-core.service";
+import {GlobalCoreService} from './shared/global-core.service';
+import {AuthFirebaseComponent} from './auth/auth-firebase/auth-firebase.component';
+import {AngularFireModule} from '@angular/fire/compat';
+import {environment} from '../environments/environment';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
 
 @NgModule({
     declarations: [
@@ -18,6 +22,7 @@ import {GlobalCoreService} from "./shared/global-core.service";
         HeaderComponent,
         SignupComponent,
         WelcomeComponent,
+        AuthFirebaseComponent,
     ],
     imports: [
         BrowserModule,
@@ -26,9 +31,11 @@ import {GlobalCoreService} from "./shared/global-core.service";
         AppRoutingModule,
         MatButtonModule,
         BrowserAnimationsModule,
+        AngularFireAuthModule,        
+        AngularFireModule.initializeApp(environment.firebaseConfig)
     ],
     providers: [
-        AuthGithubService,
+        AuthService,
         GlobalCoreService,
     ],
     bootstrap: [AppComponent]
---
+++ src/app/auth/auth-firebase/auth-firebase.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AuthFirebaseComponent } from './auth-firebase.component';
+
+describe('AuthFirebaseComponent', () => {
+  let component: AuthFirebaseComponent;
+  let fixture: ComponentFixture<AuthFirebaseComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AuthFirebaseComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AuthFirebaseComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
