f61ba57a4e1a7df1545bb14f1a4fb4a097bf2691
KozerenkoRoman <kozerenko@gmail.com>
2022-01-12 23:17:37 +0200

    feat: add authService

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {SignupComponent} from './pages/signup/signup.component';
 import {WelcomeComponent} from './pages/welcome/welcome.component';
+// import {AngularFireAuth} from '@angular/fire/compat/auth';
+// import { AngularFireAuthGuard, hasCustomClaim, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/compat/auth-guard';
+
 
 const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
