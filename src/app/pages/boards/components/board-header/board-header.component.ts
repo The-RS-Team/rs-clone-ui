b1a6e8bd396e00fd1c8b137c2d192036369bf90b
Victoria Ilyina <47503735+VictoriaIL@users.noreply.github.com>
2022-02-16 14:28:28 +0300

    feat: add an abitity to change board name

--- src/app/pages/boards/components/board-header/board-header.component.ts
+++ src/app/pages/boards/components/board-header/board-header.component.ts
@@ -1,51 +1,77 @@
-import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {BoardInterface} from "../../../../interfaces/board.interface";
+import {Messages} from "../../../../app.constants";
+import {BoardsService} from "../../boards.service";
+import {Subscription} from "rxjs";
 
 @Component({
-  selector: 'app-board-header',
-  templateUrl: './board-header.component.html',
-  styleUrls: ['./board-header.component.scss']
+    selector: 'app-board-header',
+    templateUrl: './board-header.component.html',
+    styleUrls: ['./board-header.component.scss']
 })
-export class BoardHeaderComponent implements OnInit {
-  menu: HTMLElement | undefined;
-  isMenuOpen = false;
-  isSubMenu = false;
-  settings = '';
+export class BoardHeaderComponent implements OnInit, OnDestroy {
+    menu: HTMLElement | undefined;
+    isMenuOpen = false;
+    isSubMenu = false;
+    settings = '';
 
-  constructor() { }
-
-  @ViewChild('menuWrapper') menuWrapper: ElementRef | undefined;
-
-  @Input()
-  boardWrapper: HTMLElement | undefined;
-
-  ngOnInit(): void {
-  }
-
-  ngAfterViewInit(): void {
-    this.menu = this.menuWrapper?.nativeElement;
-  }
-
-  openMenu() {
-    this.menu!.className = 'menu opened';
-    this.isMenuOpen = true;
-  }
-
-  closeMenu() {
-    this.menu!.className = 'menu';
-    this.isMenuOpen = false;
-  }
-
-  openSettings(key: string) {
-    this.isSubMenu = true;
-    switch (key) {
-      case 'bg':
-        this.settings = 'bg';
-        break;
+    constructor(private boardsService: BoardsService) {
     }
-  }
 
-  goBack(path: string) {
-    this.settings = path;
-  }
+    @ViewChild('menuWrapper') menuWrapper: ElementRef | undefined;
+
+    @Input() boardWrapper: HTMLElement | undefined;
+    @Input() board: BoardInterface | undefined;
+
+    private sub$ = new Subscription();
+
+
+    ngOnInit(): void {
+    }
+
+    ngAfterViewInit(): void {
+        this.menu = this.menuWrapper?.nativeElement;
+    }
+
+    openMenu() {
+        this.menu!.className = 'menu opened';
+        this.isMenuOpen = true;
+    }
+
+    closeMenu() {
+        this.menu!.className = 'menu';
+        this.isMenuOpen = false;
+    }
+
+    openSettings(key: string) {
+        this.isSubMenu = true;
+        switch (key) {
+            case 'bg':
+                this.settings = 'bg';
+                break;
+        }
+    }
+
+    goBack(path: string) {
+        this.settings = path;
+    }
+
+    public changeBoardTitle(value: string) {
+        if (this.board) {
+            const item = {
+                id: this.board.id,
+                title: value,
+                description: this.board.description,
+                isFavorite: this.board.isFavorite,
+                background: this.board.background,
+                columns: this.board.columns,
+            }
+            this.boardsService.updateBoard(item).subscribe(item => console.log('ddd'))
+        }
+    }
+
+    public ngOnDestroy() {
+        this.sub$.unsubscribe();
+    }
 
 }
